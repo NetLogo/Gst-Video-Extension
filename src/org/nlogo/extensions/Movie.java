ec18e40c776be66a1dd385df3015b138e0dfe11f
TheBizzle <jason.bertsche@gmail.com>
2012-07-23 16:47:32 -0500

    Disabling TextOverlay

--- src/org/nlogo/extensions/Movie.java
+++ src/org/nlogo/extensions/Movie.java
@@ -137,13 +137,13 @@ public strictfp class Movie {
 					Element caps = ElementFactory.make("capsfilter", null);
 					
 					// FPS textoverlay
-					fpsCountOverlay = ElementFactory.make("textoverlay", null);
-					fpsCountOverlay.set("text", "FPS: --");
-					fpsCountOverlay.set("font-desc", "normal 32");
-					fpsCountOverlay.set("halign", "right");
-					fpsCountOverlay.set("valign", "top");
+					//fpsCountOverlay = ElementFactory.make("textoverlay", null);
+					//fpsCountOverlay.set("text", "FPS: --");
+					//fpsCountOverlay.set("font-desc", "normal 32");
+					//fpsCountOverlay.set("halign", "right");
+					//fpsCountOverlay.set("valign", "top");
 
-					sink.addMany	(scale, caps, conv, fpsCountOverlay, appSink);
+					sink.addMany	(scale, caps, conv, appSink);
 
 					String capsString = String.format("video/x-raw-rgb, width=%d, height=%d", (int)width, (int)height);
 					Caps sizeCaps = Caps.fromString(capsString);
@@ -153,8 +153,8 @@ public strictfp class Movie {
 						System.out.println("Problem with scale->caps");
 					if (!caps.link(conv))
 						System.out.println("Problem with caps->conv");
-					if (!conv.link(fpsCountOverlay))
-						System.out.println("Problem with conv->overlay");
+				//	if (!conv.link(fpsCountOverlay))
+				//		System.out.println("Problem with conv->overlay");
 
 					List<Pad> pads = scale.getSinkPads();
 					Pad sinkPad = pads.get(0);
@@ -165,7 +165,7 @@ public strictfp class Movie {
 					// Snippet from http://opencast.jira.com/svn/MH/trunk/modules/matterhorn-composer-gstreamer/src/main/java/org/opencastproject/composer/gstreamer/engine/GStreamerEncoderEngine.java
 					Caps some_caps = new Caps("video/x-raw-rgb"
 									+ ", bpp=32, depth=32, framerate=30/1, red_mask=(int)65280, green_mask=(int)16711680, blue_mask=(int)-16777216, alpha_mask=(int)255");
-					if (!Element.linkPadsFiltered(fpsCountOverlay, "src", appSink, "sink", some_caps)) {
+					if (!Element.linkPadsFiltered(conv, "src", appSink, "sink", some_caps)) {
 						throw new ExtensionException("Failed linking ffmpegcolorspace with appsink");
 					}
 
@@ -439,7 +439,7 @@ public strictfp class Movie {
 					
 				if (System.currentTimeMillis() - prevTime >= 1000) {
 					
-					fpsCountOverlay.set("text", "FPS: " + frameCount);
+					//fpsCountOverlay.set("text", "FPS: " + frameCount);
 					
 					prevTime = System.currentTimeMillis();
 					frameCount = 0;		
